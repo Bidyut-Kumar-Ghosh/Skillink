a6a4ffc2fb48307be9d5e6b9a8cd58c24ad464a3
Bidyut <deepghosh369@gmail.com>
2025-03-19 22:11:38 +0530

    Adding App.js

---
+++ App.js
@@ -0,0 +1,7 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { View } from "react-native-web";
+
+export default function App() {
+  return {};
+}
