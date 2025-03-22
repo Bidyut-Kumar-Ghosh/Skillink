6f28c52c069a9d5e6bb3d97eaeeb2f684b0b58b8
Bidyut <deepghosh369@gmail.com>
2025-03-22 21:46:33 +0530

    Delete some Unnecesorry Files

--- App.js
+++
@@ -1,7 +0,0 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
-import { View } from "react-native-web";
-
-export default function App() {
-  return {};
-}
